2c46d71e3e76641d3fd33e24590977553438e677
jiyeon.kim <yeon19april@gmail.com>
2023-05-31 21:55:06 +0900

    feat: add contact

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,13 +1,23 @@
 "use client";
+import Image from "next/image";
+import Link from "next/link";
 import React, { useEffect } from "react";
 const Contact = (): JSX.Element => {
   useEffect(() => {}, []);
 
   return (
     <section id="contact">
-      <div className="flex justify-center py-16 text-white align-middle bg-black px-28 h-96">
-        <span className="text-sm">
-          © 2023. Jiyeon Kim. All rights reserved.
+      <div className="flex flex-col items-center justify-center py-16 text-white align-middle bg-black px-28">
+        <Link href="https://github.com/april-jiyeon-kim" target="blank">
+          <Image
+            src={"/images/git-hub.png"}
+            alt={"github"}
+            width={60}
+            height={60}
+          />
+        </Link>
+        <span className="mt-5 text-sm ">
+          ©{new Date().getFullYear()}. Jiyeon Kim. All rights reserved.
         </span>
       </div>
     </section>
