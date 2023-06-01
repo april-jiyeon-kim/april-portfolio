12c9ff25a888437f4ca6977c1aa436d098a1631b
jiyeon.kim <yeon19april@gmail.com>
2023-06-01 12:44:07 +0900

    feat: add Project Reading Habbit

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import React from "react";
+import Link from "next/link";
+import React, { useState } from "react";
 
 interface ProjectCardProps {
   title: string;
@@ -9,9 +10,10 @@ interface ProjectCardProps {
   github: string;
   url: string;
   frontend: string;
-  backend: string;
-  database: string;
-  deployment: string;
+  backend?: string;
+  database?: string;
+  deployment?: string;
+  design?: string;
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({
@@ -25,28 +27,69 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   backend,
   database,
   deployment,
+  design,
 }) => {
   return (
-    <div className="flex skill-card">
-      {previewImgs && <h1>Temp</h1>}
-      <h1 className="skill-card-title">{title}</h1>
+    <div className="flex flex-col max-w-6xl p-10 mx-auto mt-10 bg-white rounded-3xl ">
+      <div className="flex items-center justify-center w-full bg-black h-[400px]"></div>
+      <div className="mb-4 text-4xl mt-14 font-extrabol font-archivoblack">
+        {title}
+      </div>
       <div className="flex flex-row">
-        <div>{description}</div>
-        <div>
-          <div>
-            <h2>Main Information</h2>
+        <div className="w-[40%] pr-4 font-sans">{description}</div>
+        <div className="w-[60%] text-normal">
+          <div className="flex w-full">
+            <div className="inline-block w-36 archivonarrow">
+              Main Information
+            </div>
+            <div className="text-sm w-[calc(100%-10rem)]">
+              {mainInformation}
+            </div>
           </div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div className="flex w-full">
+            <div className="inline-block w-36 archivonarrow">GitHub</div>
+            <Link className="text-sm w-[calc(100%-10rem)] " href={github}>
+              {github}
+            </Link>
+          </div>
+          <div className="flex w-full">
+            <div className="inline-block w-36 archivonarrow">url</div>
+            <Link className="text-sm w-[calc(100%-10rem)] " href={url}>
+              {url}
+            </Link>
+          </div>
+          <div className="flex w-full">
+            <div className="inline-block w-36 archivonarrow">Frontend</div>
+            <div className="text-sm w-[calc(100%-10rem)] ">{frontend}</div>
+          </div>
+          {backend && (
+            <div className="flex w-full">
+              <div className="inline-block w-36 archivonarrow">Backend</div>
+              <div className="text-sm w-[calc(100%-10rem)] ">{backend}</div>
+            </div>
+          )}
+          {database && (
+            <div className="flex w-full">
+              <div className="inline-block w-36 archivonarrow">Database</div>
+              <div className="text-sm w-[calc(100%-10rem)] ">{database}</div>
+            </div>
+          )}
+          {deployment && (
+            <div className="flex w-full">
+              <div className="inline-block w-36 archivonarrow">Deployement</div>
+              <div className="text-sm w-[calc(100%-10rem)] ">{deployment}</div>
+            </div>
+          )}
+          {design && (
+            <div className="flex w-full">
+              <div className="inline-block w-36 archivonarrow">Design</div>
+              <div className="text-sm w-[calc(100%-10rem)] ">{design}</div>
+            </div>
+          )}
         </div>
       </div>
       <div></div>
     </div>
   );
 };
-
 export default ProjectCard;
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,14 +1,30 @@
 "use client";
 
 import React, { useEffect } from "react";
+import ProjectCard from "./ProjectCard";
 
 const Projects = (): JSX.Element => {
   useEffect(() => {}, []);
 
   return (
     <section id="projects">
-      <div className="flex justify-center py-16 align-middle px-28 h-96 bg-green ">
-        <h1 className="mb-6 text-4xl font-bold text-white">PROJECTS</h1>
+      <div className="flex flex-col items-center justify-center px-6 py-16 bg-green ">
+        <h1 className="mb-6 text-4xl font-bold text-white font-archivoblack">
+          PROJECTS
+        </h1>
+        <div>
+          <ProjectCard
+            title="Reading Habbit"
+            description="I recently developed ReadingHabit, a mobile app designed to help users cultivate a daily reading habit. The app provides a simple and intuitive interface to track reading progress, set goals, and get personalized recommendations based on reading history and preferences. It was a valuable experience for me to have the opportunity to directly use the Figma tool, which I had previously only used in collaboration with designers."
+            mainInformation={`Log the number of books they've read and the percentage of their reading goals they've achieved Keep track of their reading habits in the form of a pie chart, Record their favorite quotes and notes for each book, Set wish books and add books to their reading list, keep track of the books they're currently reading and the books they've finished reading `}
+            github="https://github.com/april-jiyeon-kim/reading-habbit"
+            url=""
+            frontend="React Native"
+            backend="Firebase"
+            database="Firebase Firestore"
+            design="Figma"
+          />
+        </div>
       </div>
     </section>
   );
