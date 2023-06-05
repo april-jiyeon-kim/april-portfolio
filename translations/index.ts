a4e84b4b99a1128f6a79b1f777e964b0de0e75de
jiyeon.kim <yeon19april@gmail.com>
2023-06-05 14:47:17 +0900

    feat: set default language english

--- translations/index.ts
+++ translations/index.ts
@@ -24,28 +24,27 @@ export const preferredRequestLanguage = (
     );
 
   // Next, check for a `language` cookie
-  const languageCookie = cookies().get("language");
+  // const languageCookie = cookies().get("language");
 
-  console.log(languageCookie);
-  if (languageCookie !== undefined)
-    return (
-      supportedLanguages.find(
-        (language) => language === languageCookie.value
-      ) || fallbackLanguage
-    );
+  // if (languageCookie !== undefined)
+  //   return (
+  //     supportedLanguages.find(
+  //       (language) => language === languageCookie.value
+  //     ) || fallbackLanguage
+  //   );
 
   // No luck so far? Continue by checking the `Accept-Language` header
-  const acceptLanguageHeader = headers().get("Accept-Language");
+  // const acceptLanguageHeader = headers().get("Accept-Language");
 
-  if (acceptLanguageHeader === null) return fallbackLanguage;
+  // if (acceptLanguageHeader === null) return fallbackLanguage;
 
-  const acceptedLanguages = parse(acceptLanguageHeader);
-  for (const acceptedLanguage of acceptedLanguages) {
-    const preferredLanguage = supportedLanguages.find(
-      (language) => language === acceptedLanguage.code
-    );
-    if (preferredLanguage !== undefined) return preferredLanguage;
-  }
+  // const acceptedLanguages = parse(acceptLanguageHeader);
+  // for (const acceptedLanguage of acceptedLanguages) {
+  //   const preferredLanguage = supportedLanguages.find(
+  //     (language) => language === acceptedLanguage.code
+  //   );
+  //   if (preferredLanguage !== undefined) return preferredLanguage;
+  // }
 
   return fallbackLanguage;
 };
