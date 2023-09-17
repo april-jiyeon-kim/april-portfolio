9a1c7f64b17c3c979a7fb6ba497b81b974a59c81
jiyeon.kim <yeon19april@gmail.com>
2023-09-17 17:31:07 +0900

    chore: setup test

---
+++ app/components/AboutMe.test.tsx
@@ -0,0 +1,12 @@
+import { describe, it, expect } from "vitest";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import AboutMe from "./AboutMe";
+
+describe("AboutMe", () => {
+  it("renders about me", () => {
+    render(<AboutMe />);
+    expect(screen.getByText("About me")).toBeInTheDocument();
+  });
+});
