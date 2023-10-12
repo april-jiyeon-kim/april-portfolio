0309bab80aa876bf75d5f2e059469904eac732a2
april-jiyeon-kim <yeon19april@gmail.com>
2023-10-12 16:28:33 +0900

    feat: use hook 사용

--- app/oauth/success/page.tsx
+++ app/oauth/success/page.tsx
@@ -1,16 +1,21 @@
 "use client";
 import { useSearchParams } from "next/navigation";
+import { useCallback, useState } from "react";
 
 export default function Success() {
   const searchParams = useSearchParams();
   const code = searchParams.get("code");
   const mallId = searchParams.get("state");
+  const [accessToken, setAccessToken] = useState("");
+
+  const fetchData = useCallback(() => {
+    console.log(code, mallId);
+  }, [code, mallId]);
+
   return (
     <div>
       <h1>인증 코드 발급 성공</h1>
-      <button onClick={() => console.log(code, mallId)}>
-        Access token 발급하기
-      </button>
+      <button onClick={fetchData}>Access token 발급하기</button>
     </div>
   );
 }
