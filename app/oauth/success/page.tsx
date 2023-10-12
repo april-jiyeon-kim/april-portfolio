01765843cc0f92f626b4360c7e3076c29e191eb1
april-jiyeon-kim <yeon19april@gmail.com>
2023-10-12 16:21:24 +0900

    feat: success 페이지 테스트

--- app/oauth/success/page.tsx
+++ app/oauth/success/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+
 export default function Success() {
+  const searchParams = useSearchParams();
+  const code = searchParams.get("code");
+  const mallId = searchParams.get("state");
   return (
     <div>
-      <h1>Success</h1>
+      <h1>인증 코드 발급 성공</h1>
+      <button onClick={() => console.log(code, mallId)}>
+        Access token 발급하기
+      </button>
     </div>
   );
 }
